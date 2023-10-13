aff8cbdf5ef8a8b85907739e64cea6e18b224bb9
Rafael <rafaelpinheiro2273@gmail.com>
2023-10-13 12:00:30 -0300

    create models and migration

--- cbo/models.py
+++ cbo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 class BaseModel(models.Model):
@@ -11,144 +11,138 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class USUARIO(AbstractUser):
-    NO_USUARIO = models.CharField(max_length=55)
-    NO_SOBRENOME_USUARIO = models.CharField(max_length=55)
+class User(BaseModel):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
     CPF = models.CharField(max_length=11)
-    ESPECIALIDADE = models.CharField(max_length=255)
-    EMAIL = models.CharField(max_length=55)
-    TELEFONE = models.CharField(max_length=15)
-    DT_NASCIMENTO = models.DateField()
+    specialty = models.CharField(max_length=255)
+    email = models.CharField(max_length=55)
+    telephone = models.CharField(max_length=15)
+    date_of_birth = models.DateField()
     CRM = models.CharField(max_length=15)
-    created_at = models.DateTimeField(auto_now_add=True)
-    last_update = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(null=True, blank=True)
 
 
-class CID(BaseModel):
-    PROCEDIMENTOS_id = models.IntegerField(null=False)
-    CO_CID = models.CharField(max_length=4, null=False)
-    NO_CID = models.CharField(max_length=100, null=False)
-    TP_AGRAVO = models.CharField(max_length=1, null=False)
-    TP_SEXO = models.CharField(max_length=1, null=False)
-    TP_ESTADIO = models.CharField(max_length=1, null=False)
-    VL_CAMPOS_IRRADIADOS = models.IntegerField(null=False)
+class Occupation(BaseModel):
+    occupation_code = models.CharField(max_length=6, primary_key=True)
+    name = models.CharField(max_length=150, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='occupations')
 
 
-class CID_HISTORICO(BaseModel):
-    PROCEDIMENTOS_id = models.IntegerField(null=False)
-    CO_CID = models.CharField(max_length=4, null=False)
-    NO_CID = models.CharField(max_length=100, null=False)
-    TP_AGRAVO = models.CharField(max_length=1, null=False)
-    TP_SEXO = models.CharField(max_length=1, null=False)
-    TP_ESTADIO = models.CharField(max_length=1, null=False)
-    VL_CAMPOS_IRRADIADOS = models.IntegerField(null=False)
-    CID_id = models.IntegerField(null=False)
-    ULTIMA_ATUALIZACAO = models.DateField(null=False)
+class Occupation_history(BaseModel):
+    occupation_code = models.CharField(max_length=6, primary_key=True)
+    name = models.CharField(max_length=150, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='occupations_history')
+    occupation = models.ForeignKey(Occupation, on_delete=models.CASCADE, null=True, related_name='occupations_history')
 
 
-class OCUPACAO(BaseModel):
-    CO_OCUPACAO = models.CharField(max_length=6, null=False)
-    NO_OCUPACAO = models.CharField(max_length=150, null=False)
-    USUARIO_id = models.IntegerField(null=False)
+class Record(BaseModel):
+    record_code = models.CharField(max_length=2, primary_key=True)
+    name = models.CharField(max_length=50, null=False)
+    competence_date = models.CharField(max_length=6, null=False)
 
 
-class OCUPACAO_HISTORICO(BaseModel):
-    CO_OCUPACAO = models.CharField(max_length=6, null=False)
-    NO_OCUPACAO = models.CharField(max_length=150, null=False)
-    ULTIMA_ATUALIZACAO = models.DateField(null=False)
-    OCUPACAO_id = models.IntegerField(null=False)
+class Record_history(BaseModel):
+    record_code = models.CharField(max_length=2, primary_key=True)
+    name = models.CharField(max_length=50, null=False)
+    competence_date = models.CharField(max_length=6, null=False)
+    record = models.ForeignKey(Record, on_delete=models.CASCADE, null=True, related_name='records_history')
 
 
-class PROCEDIMENTOS(BaseModel):
-    REGISTROS_id = models.IntegerField(null=False)
-    NO_PROCEDIMENTO = models.CharField(max_length=250, null=False)
-    TP_COMPLEXIDADE = models.CharField(max_length=1, null=False)
-    TP_SEXO = models.CharField(max_length=1, null=False)
-    QT_MAXIMA_EXECUCAO = models.IntegerField(null=False)
-    QT_DIAS_PERMANENCIA = models.CharField(max_length=4, null=False)
-    QT_PONTOS = models.IntegerField(null=False)
-    VL_IDADE_MINIMA = models.IntegerField(null=False)
-    VL_IDADE_MAXIMA = models.IntegerField(null=False)
-    VL_SH = models.IntegerField(null=False)
-    VL_SA = models.IntegerField(null=False)
-    VL_SP = models.IntegerField(null=False)
-    QT_TEMPO_PERMANENCIA = models.IntegerField(null=False)
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
+class Procedure(BaseModel):
+    procedure_code = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=250, null=True)
+    complexity_type = models.CharField(max_length=1, null=True)
+    sex_type = models.CharField(max_length=1, null=True)
+    maximum_execution_amount = models.IntegerField(null=True)
+    stay_day_number = models.CharField(max_length=4, null=True)
+    points_number = models.IntegerField(null=True)
+    minimum_age_value = models.IntegerField(null=True)
+    maximum_age_value = models.IntegerField(null=True)
+    SH_value = models.IntegerField(null=True)
+    SA_value = models.IntegerField(null=True)
+    SP_value = models.IntegerField(null=True)
+    stay_time_number = models.IntegerField(null=True)
+    competence_date = models.CharField(max_length=6, null=True)
+    record = models.ForeignKey(Record, on_delete=models.CASCADE, null=True, related_name='procedures')
 
 
-class PROCEDIMENTOS_HISTORICO(BaseModel):
-    REGISTROS_id = models.IntegerField(null=False)
-    NO_PROCEDIMENTO = models.CharField(max_length=250, null=False)
-    TP_COMPLEXIDADE = models.CharField(max_length=1, null=False)
-    TP_SEXO = models.CharField(max_length=1, null=False)
-    QT_MAXIMA_EXECUCAO = models.IntegerField(null=False)
-    QT_DIAS_PERMANENCIA = models.CharField(max_length=4, null=False)
-    QT_PONTOS = models.IntegerField(null=False)
-    VL_IDADE_MINIMA = models.IntegerField(null=False)
-    VL_IDADE_MAXIMA = models.IntegerField(null=False)
-    VL_SH = models.IntegerField(null=False)
-    VL_SA = models.IntegerField(null=False)
-    VL_SP = models.IntegerField(null=False)
-    QT_TEMPO_PERMANENCIA = models.IntegerField(null=False)
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
-    ULTIMA_ATUALIZACAO = models.DateField(null=False)
-    PROCEDIMENTOS_id = models.IntegerField(null=False)
+class Procedure_history(BaseModel):
+    procedure_code = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=250, null=True)
+    complexity_type = models.CharField(max_length=1, null=True)
+    sex_type = models.CharField(max_length=1, null=True)
+    maximum_execution_amount = models.IntegerField(null=True)
+    stay_day_number = models.CharField(max_length=4, null=True)
+    points_number = models.IntegerField(null=True)
+    minimum_age_value = models.IntegerField(null=True)
+    maximum_age_value = models.IntegerField(null=True)
+    SH_value = models.IntegerField(null=True)
+    SA_value = models.IntegerField(null=True)
+    SP_value = models.IntegerField(null=True)
+    stay_time_number = models.IntegerField(null=True)
+    competence_date = models.CharField(max_length=6, null=True)
+    record = models.ForeignKey(Record_history, on_delete=models.CASCADE, null=True, related_name='procedures_history')
+    procedure = models.ForeignKey(Procedure, on_delete=models.CASCADE, null=True, related_name='procedures_history')
 
 
-class REGISTROS(BaseModel):
-    CO_REGISTRO = models.CharField(max_length=2, null=False)
-    NO_REGISTRO = models.CharField(max_length=50, null=False)
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
+class Cid(BaseModel):
+    cid_code = models.CharField(max_length=4, primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    grievance_type = models.CharField(max_length=1, null=False)
+    sex_type = models.CharField(max_length=1, null=False)
+    stadium_stype = models.CharField(max_length=1, null=False)
+    irradiated_fields_value = models.IntegerField(null=False)
+    procedure = models.ForeignKey(Procedure, on_delete=models.CASCADE, null=True, related_name='cids')
 
 
-class REGISTROS_HISTORICO(BaseModel):
-    CO_REGISTRO = models.CharField(max_length=2, null=False)
-    NO_REGISTRO = models.CharField(max_length=50, null=False)
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
-    ULTIMA_ATUALIZACAO = models.DateField(null=False)
-    REGISTROS_id = models.IntegerField(null=False)
+class Cid_history(BaseModel):
+    cid_code = models.CharField(max_length=4, primary_key=True)
+    name = models.CharField(max_length=100, null=False)
+    grievance_type = models.CharField(max_length=1, null=False)
+    sex_type = models.CharField(max_length=1, null=False)
+    stadium_stype = models.CharField(max_length=1, null=False)
+    irradiated_fields_value = models.IntegerField(null=False)
+    procedure = models.ForeignKey(Procedure_history, on_delete=models.CASCADE, null=True, related_name='cids_history')
+    cid = models.ForeignKey(Cid, on_delete=models.CASCADE, null=True, related_name='cids_history')
 
 
-class RL_PROCEDIMENTO_CID(BaseModel):
-    ST_PRINCIPAL = models.CharField(max_length=4, null=False)
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
-    CID_id = models.IntegerField(null=False)
-    PROCEDIMENTOS_id = models.IntegerField(null=False)
+class Procedure_has_cid(BaseModel):
+    st_principal = models.CharField(max_length=4, null=False)
+    competence_date = models.CharField(max_length=6, null=False)
+    procedure = models.ForeignKey(Procedure, on_delete=models.CASCADE, null=True, related_name='procedures_has_cid')
+    cid = models.ForeignKey(Cid, on_delete=models.CASCADE, null=True, related_name='cids_has_procedure')
 
 
-class RL_PROCEDIMENTO_CID_HISTORICO(BaseModel):
-    ST_PRINCIPAL = models.CharField(max_length=4, null=False)
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
-    ULTIMA_ATUALIZACAO = models.DateField(null=False)
-    CID_id = models.IntegerField(null=False)
-    PROCEDIMENTOS_id = models.IntegerField(null=False)
-    RL_PROCEDIMENTO_CID_id = models.IntegerField(null=False)
+class Procedure_has_cid_history(BaseModel):
+    st_principal = models.CharField(max_length=4, null=False)
+    competence_date = models.CharField(max_length=6, null=False)
+    procedure = models.ForeignKey(Procedure_history, on_delete=models.CASCADE, null=True, related_name='procedures_has_cid')
+    cid = models.ForeignKey(Cid_history, on_delete=models.CASCADE, null=True, related_name='cids_has_procedure')
+    procedure_has_cid = models.ForeignKey(Procedure_has_cid, on_delete=models.CASCADE, null=True)
 
 
-class RL_PROCEDIMENTO_OCUPACAO(BaseModel):
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
-    PROCEDIMENTOS_id = models.IntegerField(null=False)
-    OCUPACAO_id = models.IntegerField(null=False)
+class Procedure_has_occupation(BaseModel):
+    competence_date = models.CharField(max_length=6, null=False)
+    procedure = models.ForeignKey(Procedure, on_delete=models.CASCADE, null=True, related_name='procedures_has_occupation')
+    occupation = models.ForeignKey(Occupation, on_delete=models.CASCADE, null=True, related_name='occupations_has_procedure')
 
 
-class RL_PROCEDIMENTO_OCUPACAO_HISTORICO(BaseModel):
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
-    PROCEDIMENTOS_id = models.IntegerField(null=False)
-    OCUPACAO_id = models.IntegerField(null=False)
-    ULTIMA_ATUALIZACAO = models.DateField(null=False)
-    RL_PROCEDIMENTO_OCUPACAO_id = models.IntegerField(null=False)
+class Procedure_has_occupation_history(BaseModel):
+    competence_date = models.CharField(max_length=6, null=False)
+    procedure = models.ForeignKey(Procedure_history, on_delete=models.CASCADE, null=True, related_name='procedures_has_occupation')
+    occupation = models.ForeignKey(Occupation_history, on_delete=models.CASCADE, null=True, related_name='occupations_has_procedure')
+    procedure_has_occupation = models.ForeignKey(Procedure_has_occupation, on_delete=models.CASCADE, null=True)
 
 
-class RL_PROCEDIMENTO_REGISTRO(BaseModel):
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
-    REGISTROS_id = models.IntegerField(null=False)
-    PROCEDIMENTOS_id = models.IntegerField(null=False)
+class Procedure_has_record(BaseModel):
+    competence_date = models.CharField(max_length=6, null=False)
+    procedure = models.ForeignKey(Procedure, on_delete=models.CASCADE, null=True, related_name='procedures_has_record')
+    record = models.ForeignKey(Record, on_delete=models.CASCADE, null=True, related_name='records_has_procedure')
 
 
-class RL_PROCEDIMENTO_REGISTRO_HISTORICO(BaseModel):
-    DT_COMPETENCIA = models.CharField(max_length=6, null=False)
-    REGISTROS_id = models.IntegerField(null=False)
-    PROCEDIMENTOS_id = models.IntegerField(null=False)
-    ULTIMA_ATUALIZACAO = models.DateField(null=False)
-    RL_PROCEDIMENTO_REGISTRO_id = models.IntegerField(null=False)
+class Procedure_has_record_history(BaseModel):
+    competence_date = models.CharField(max_length=6, null=False)
+    procedure = models.ForeignKey(Procedure_history, on_delete=models.CASCADE, null=True, related_name='procedures_has_record')
+    record = models.ForeignKey(Record_history, on_delete=models.CASCADE, null=True, related_name='records_has_procedure')
+    procedure_has_record = models.ForeignKey(Procedure_has_record, on_delete=models.CASCADE, null=True)
