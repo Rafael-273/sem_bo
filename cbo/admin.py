bb95e4917fb5fd7ddab679906b1d2c9193020fe9
Rafael <rafaelpinheiro2273@gmail.com>
2023-10-13 12:10:04 -0300

    create admin.py

--- cbo/admin.py
+++ cbo/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import User, Occupation, Occupation_history, Record, Record_history, Procedure, Procedure_history, Cid, Cid_history, Procedure_has_cid, Procedure_has_cid_history, Procedure_has_occupation, Procedure_has_occupation_history, Procedure_has_record, Procedure_has_record_history
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Occupation)
+admin.site.register(Occupation_history)
+admin.site.register(Record)
+admin.site.register(Record_history)
+admin.site.register(Procedure)
+admin.site.register(Procedure_history)
+admin.site.register(Cid)
+admin.site.register(Cid_history)
+admin.site.register(Procedure_has_cid)
+admin.site.register(Procedure_has_cid_history)
+admin.site.register(Procedure_has_occupation)
+admin.site.register(Procedure_has_occupation_history)
+admin.site.register(Procedure_has_record)
+admin.site.register(Procedure_has_record_history)
