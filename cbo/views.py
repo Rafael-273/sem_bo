a50f20171430299bcb17e586baa920faad2f3985
Rafael <rafaelpinheiro2273@gmail.com>
2023-11-06 18:33:06 -0300

    feature infinite search

--- cbo/views.py
+++ cbo/views.py
@@ -7,6 +7,10 @@ from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.db.models import Q
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 
 class UploadFilesView(View):
@@ -50,9 +54,26 @@ class SearchView(View):
     def get(self, request, *args, **kwargs):
         query = request.GET.get('q')
         if query:
-            procedures = Procedure.objects.filter(Q(name__icontains=query)).prefetch_related('procedures_has_record__record')
-
+            procedures_list = Procedure.objects.filter(Q(name__icontains=query)).prefetch_related('procedures_has_record__record')
+            page = request.GET.get('page', 1)
+            paginator = Paginator(procedures_list, 20)
+            try:
+                procedures = paginator.page(page)
+            except PageNotAnInteger:
+                procedures = paginator.page(1)
+            except EmptyPage:
+                procedures = paginator.page(paginator.num_pages)
 
             return render(request, 'front/search_results.html', {'procedures': procedures})
-        else:
-            return render(request, 'front/home.html')
+
+
+
+
+        # query = request.GET.get('q')
+        # if query:
+        #     procedures = Procedure.objects.filter(Q(name__icontains=query)).prefetch_related('procedures_has_record__record')
+
+
+        #     return render(request, 'front/search_results.html', {'procedures': procedures[:7]})
+        # else:
+        #     return render(request, 'front/home.html')
